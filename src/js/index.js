84cb73570b34706915306d2dd34b1c047c312965
Maxime Rouyrre <rouyrre+git@gmail.com>
2017-05-14 22:38:30 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,85 @@
+const width = 400
+const height = 300
+
+var current = Array(height).fill(0).map(x => Array(width).fill(0))
+var next = Array(height).fill(0).map(x => Array(width).fill(0))
+
+current[149][210] = 1
+current[150][212] = 1
+current[151][209] = 1
+current[151][210] = 1
+current[151][213] = 1
+current[151][214] = 1
+current[151][215] = 1
+
+const canvas = document.getElementById('worldmap')
+const context = canvas.getContext('2d')
+const info = document.getElementById('info')
+var time = 0
+var then = Date.now()
+var fps = 0
+var neighbors = 0
+
+function cell (i, j) {
+  if (i === -1) {
+    i = height - 1
+  } else if (i === height) {
+    i = 0
+  }
+  if (j === -1) {
+    j = width - 1
+  } else if (j === width) {
+    j = 0
+  }
+  return current[i][j]
+}
+
+function computeNextStep () {
+  for (var i = 0; i < height; i++) {
+    for (var j = 0; j < width; j++) {
+      neighbors = cell(i - 1, j - 1) + cell(i - 1, j) + cell(i - 1, j + 1)
+      neighbors += cell(i, j - 1) + cell(i, j + 1)
+      neighbors += cell(i + 1, j - 1) + cell(i + 1, j) + cell(i + 1, j + 1)
+      if (current[i][j] === 0 && neighbors === 3) {
+        next[i][j] = 1
+      } else if (current[i][j] === 1 && (neighbors === 2 || neighbors === 3)) {
+        next[i][j] = 1
+      } else {
+        next[i][j] = 0
+      }
+    }
+  }
+};
+
+function draw (timeStamp) {
+  context.fillStyle = 'rgb(240,240,240)'
+  context.fillRect(0, 0, 2 * width, 2 * height)
+  context.fillStyle = 'rgb(40,40,40)'
+  current.forEach(function (row, i) {
+    row.forEach(function (value, j) {
+      if (value === 1) {
+        context.fillRect(2 * j, 2 * i, 2, 2)
+      }
+    })
+  })
+  computeNextStep()
+  for (var i = 0; i < height; i++) {
+    current[i] = next[i].slice(0)
+  }
+
+  // Update FPS display every half second
+  const now = Date.now()
+  const elapsed = timeStamp - time
+  if ((now - then) > 500) {
+    fps = 1000 / elapsed
+    fpsNode.nodeValue = `${fps.toFixed(2)} FPS`
+    then = now
+  }
+  time = timeStamp
+
+  window.requestAnimationFrame(draw)
+};
+
+const fpsNode = document.createTextNode('')
+info.appendChild(fpsNode)
+window.requestAnimationFrame(draw)
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'css/[name].css?[contenthash:8]',
+  disable: process.env.NODE_ENV === 'development'
+})
+
+module.exports = function (env) {
+  return {
+    entry: {
+      bundle: './src/js/index.js',
+      styles: './src/styles/main.scss'
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '',
+      filename: 'js/[name].js?[chunkhash:8]'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.(js|jsx)$/,
+          use: 'babel-loader'
+        }, {
+          test: /[.]html$/,
+          loader: 'html-loader?' + JSON.stringify({ pretty: true })
+        }, {
+          test: /\.scss$/,
+          use: extractSass.extract({
+            use: ['css-loader', 'sass-loader?sourceMap'],
+            fallback: 'style-loader',
+            publicPath: '../'
+          })
+        }, {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff' +
+            '&hash=sha512&digest=hex&name=fonts/[name].[ext]?[hash:8]'
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader?hash=sha512&digest=hex&name=fonts/[name].[ext]?[hash:8]'
+        }, {
+          test: /\.(jpe?g|png|gif|svg|ico)$/,
+          loaders: [
+            'file-loader?hash=sha512&digest=hex&name=images/[name].[ext]?[hash:8]'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      extractSass,
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
+    ]
+  }
+}
