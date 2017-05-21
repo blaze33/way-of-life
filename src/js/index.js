dfeca2ea967f9168a4d5fd36d9372db421888534
Maxime Rouyrre <rouyrre+git@gmail.com>
2017-05-21 22:48:03 +0200

    Fixed events not being exported

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,10 @@
 import Engine from './engine'
 import Renderer from './renderer'
 import {acorn, cross, erase} from './patterns'
+import MouseEventHandler from './events'
 
 export {
   Engine, Renderer,
-  acorn, cross, erase
+  acorn, cross, erase,
+  MouseEventHandler
 }
